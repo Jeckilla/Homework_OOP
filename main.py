e9cf99e2bae99672db0c14fe3c196bbd0d3e4d63
Evgeniya Biryulina <jeckilla@gmail.com>
2023-03-08 23:39:24 +0300

    added __lt__ in main.py

--- main.py
+++ main.py
@@ -27,9 +27,16 @@ class Student:
                 return average_grades  # расчёт среднего балла
         return "No grades"
 
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Different class')
+            return 'error'
+        return self.avg_grades() < other.avg_grades()
+
     def __str__(self):
         return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашнее задание: {self.avg_grades()}\n" \
-               f"Курсы в процессе изучения: {(', ').join(self.courses_in_progress)}\nЗавершенные курсы: {(', ').join(self.finished_courses)}\n"
+               f"Курсы в процессе изучения: {(', ').join(self.courses_in_progress)}\n" \
+               f"Завершенные курсы: {(', ').join(self.finished_courses)}\n"
 
 
 class Mentor:
@@ -55,6 +62,12 @@ class Lecturer(Mentor):
                 rates_lecturer = round(sum(all_rates) / len(all_rates), 1)  # расчёт среднего балла
                 return rates_lecturer
         return "No rates"
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Different class')
+            return 'error'
+        return self.avg_rate_lecturer() < other.avg_rate_lecturer()
 
     def __str__(self):
         return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекцию: {self.avg_rate_lecturer()}\n"
@@ -82,20 +95,35 @@ best_student.grades['Python'] = [10, 10, 10, 10]
 
 simple_student = Student('Jack', 'Black', 'men')
 simple_student.courses_in_progress += ['Git']
+simple_student.courses_in_progress += ['Введение в программирование']
 simple_student.finished_courses += ['Основы Python']
 simple_student.grades['Git'] = [8, 7, 8, 9, 10]
 
 cool_lecturer = Lecturer('Some', 'Buddy')
 cool_lecturer.attached_courses += ['Python']
 
+simple_lecturer = Lecturer('Jhon', 'Thramp')
+simple_lecturer.attached_courses += ['Введение в программирование']
+
 best_student.rate_hw_lect(cool_lecturer, 'Python', 10)
 best_student.rate_hw_lect(cool_lecturer, 'Python', 9)
 best_student.rate_hw_lect(cool_lecturer, 'Python', 9)
 best_student.rate_hw_lect(cool_lecturer, 'Python', 10)
+
+simple_student.rate_hw_lect(simple_lecturer, 'Введение в программирование', 8)
+simple_student.rate_hw_lect(simple_lecturer, 'Введение в программирование', 7)
+simple_student.rate_hw_lect(simple_lecturer, 'Введение в программирование', 7)
+simple_student.rate_hw_lect(simple_lecturer, 'Введение в программирование', 10)
 
 other_mentor = Reviewer('Sam', 'Smith')
 other_mentor.attached_courses += ['Git']
 
 print(best_student)
+print(simple_student)
 print(other_mentor)
 print(cool_lecturer)
+
+print(simple_lecturer > cool_lecturer)
+print(best_student < simple_student)
+print(simple_lecturer < cool_lecturer)
+print(best_student > simple_student)
